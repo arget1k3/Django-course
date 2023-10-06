fb7085c8ca2bf5681cb63f93452f766447543506
Matvei <moty15@icloud.com>
2023-10-06 10:05:36 +0300

    add link to button and develop def, wich return id

--- djsite/coolsite/women/views.py
+++ djsite/coolsite/women/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}
+]
 
 from .models import *
 
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    context =  {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+        }
+    
+    return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
-def categories(request, catid):
-    if(request.POST):
-        print(request.POST) # http://127.0.0.1:8000/cats/1/?name=Gagarina&type=pop
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
+def addpage(request):
+    return HttpResponse('Добавление статьи')
 
-def archive(request, year):
-    if int(year)>2023:
-        return redirect('home', permanent=False)
-    return HttpResponse(f'<h1>Архив по городам</h1><p>{year}</p>') # http://127.0.0.1:8000/archive/2023/
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+def login(request):
+    return HttpResponse('Авторизация')
+
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id={post_id}')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
