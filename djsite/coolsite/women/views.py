8c3483131be042272321ca508120ada88708e22d
Matvei <moty15@icloud.com>
2023-10-13 20:38:54 +0300

    Create Call Back, add captcha

--- djsite/coolsite/women/views.py
+++ djsite/coolsite/women/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.views.generic.edit import FormView
 
 from .forms import *
 from .models import *
@@ -47,9 +48,23 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         c_def = self.get_user_context(title='Добавление статьи')
         return dict(list(context.items()) + list(c_def.items()))
 
-def contact(request):
-    return HttpResponse('Обратная связь')
+# def contact(request):
+#     return HttpResponse('Обратная связь')
 
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'women/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Обратная связь')
+        return dict(list(context.items()) + list(c_def.items()))
+    
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
+    
 # def login(request):
 #     return HttpResponse('Авторизация')
 
